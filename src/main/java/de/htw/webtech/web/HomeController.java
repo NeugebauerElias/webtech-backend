68bd1483c3a6b43b5771c7adf91d3a9c99b6f0d3
Jeff <jeff.edjekoomhene@student.htw-berlin.de>
2022-04-13 14:50:49 +0200

    Add helloworld method

---
+++ src/main/java/de/htw/webtech/web/HomeController.java
@@ -0,0 +1,15 @@
+package de.htw.webtech.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView showHelloWorld() {
+        return new ModelAndView("home");
+    }
+
+}
