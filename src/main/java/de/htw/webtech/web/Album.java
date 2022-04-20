08ea65ad97f2146ffed91e859cca7c85dc9c5101
Elias Neugebauer <eliasneugebauer2001@gmail.com>
2022-04-20 14:35:16 +0200

    User and Album class added

--- src/main/java/de/htw/webtech/web/Album.java
+++ src/main/java/de/htw/webtech/web/Album.java
@@ -1,4 +1,16 @@
 package de.htw.webtech.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Album {
+
+    int albumID;
+    String albumName;
+    List<Card> cardList;
+
+    public Album(String albumName) {
+        this.albumName = albumName;
+        cardList = new ArrayList<>();
+    }
 }
