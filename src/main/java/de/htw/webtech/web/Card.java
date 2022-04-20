75d4e3ef1ced30b1f25f0d3c0c4f90b2e1dfdfc7
Jeff <j.edjekoomhene@gmail.com>
2022-04-20 14:23:51 +0200

    Add card attributs and getter and setter

--- src/main/java/de/htw/webtech/web/Card.java
+++ src/main/java/de/htw/webtech/web/Card.java
@@ -1,4 +1,29 @@
 package de.htw.webtech.web;
 
 public class Card {
+
+    private int id;
+    private String frontInformation;
+    private String backInformation;
+
+    public Card(String frontInformation, String backInformation) {
+        this.frontInformation = frontInformation;
+        this.backInformation = backInformation;
+    }
+
+    public String getFrontInformation() {
+        return frontInformation;
+    }
+
+    public String getBackInformation() {
+        return backInformation;
+    }
+
+    public void setFrontInformation(String frontInformation) {
+        this.frontInformation = frontInformation;
+    }
+
+    public void setBackInformation(String backInformation) {
+        this.backInformation = backInformation;
+    }
 }
