ec58a17fb64186b795a60d9a1cf146eb4df44c9a
Elias Neugebauer <eliasneugebauer2001@gmail.com>
2022-04-27 14:34:05 +0200

    Test User in UserRestController

--- src/main/java/de/htw/webtech/web/UserRestController.java
+++ src/main/java/de/htw/webtech/web/UserRestController.java
@@ -1,7 +1,25 @@
 package de.htw.webtech.web;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserRestController {
+
+    private List<User> users;
+
+    public UserRestController() {
+        users =new ArrayList<>();
+        users.add(new User(1, "Max", "max@web.de", "123"));
+        users.add(new User(2, "Lilly", "lilly@web.de", "456"));
+    }
+
+    @GetMapping(path = "/api/user")
+    public ResponseEntity<List<User>> fetchUsers(){
+        return ResponseEntity.ok(users);
+    }
 }
