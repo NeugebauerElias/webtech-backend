e75b0a10a71a9f1174e6dd801248f00e1562caa8
Jeff <j.edjekoomhene@gmail.com>
2022-05-04 14:40:08 +0200

    Added Entity to Card.java

--- src/main/java/de/htw/webtech/web/Card.java
+++ src/main/java/de/htw/webtech/web/Card.java
@@ -1,13 +1,33 @@
 package de.htw.webtech.web;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Card {
 
-    private int id;
+    private Long id;
     private String frontInformation;
     private String backInformation;
 
     public Card(String frontInformation, String backInformation) {
         this.frontInformation = frontInformation;
+        this.backInformation = backInformation;
+    }
+
+    public Card() {
+
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFrontInformation() {
@@ -25,4 +45,5 @@ public class Card {
     public void setBackInformation(String backInformation) {
         this.backInformation = backInformation;
     }
+
 }
