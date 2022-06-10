2e5340d132ad619b2703c8ee09c637ad40c2fa30
Jeff <j.edjekoomhene@gmail.com>
2022-06-10 22:34:04 +0200

    Implement get method in album controller class

--- src/main/java/de/htw/webtech/web/AlbumRestController.java
+++ src/main/java/de/htw/webtech/web/AlbumRestController.java
@@ -1,4 +1,21 @@
 package de.htw.webtech.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class AlbumRestController {
+
+    @Autowired
+    public AlbumService albumService;
+
+    public AlbumRestController(AlbumService albumService) {
+        this.albumService = albumService;
+    }
+
+    @GetMapping("/album/{id}")
+    public Album getAlbum(@PathVariable String id){
+        Long albumId = Long.parseLong(id);
+        return albumService.get(albumId);
+    }
 }
