df80fe7f328efbd506170541389e48343f19d496
Jeff <j.edjekoomhene@gmail.com>
2022-06-10 22:23:37 +0200

    Implement some methods in album service

--- src/main/java/de/htw/webtech/web/AlbumService.java
+++ src/main/java/de/htw/webtech/web/AlbumService.java
@@ -1,4 +1,30 @@
 package de.htw.webtech.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class AlbumService {
+    @Autowired
+    private final AlbumRepository albumRepository;
+
+    public AlbumService(AlbumRepository albumRepository) {
+        this.albumRepository = albumRepository;
+    }
+    public Album save(Album album) {
+        return albumRepository.save(album);
+    }
+
+    public Album get(Long id) {
+        return albumRepository.findById(id).orElseThrow(() -> new RuntimeException());
+    }
+
+    public List<Album> findAll(){
+        List<Album> alben = (List<Album>) albumRepository.findAll();
+        if(alben != null) {
+            return alben;
+        }else{
+            throw new RuntimeException("No Alben exist");
+        }
+    }
 }
