d57e7c9d707038680bdd7190199f3f0e7386f1e0
Jeff <j.edjekoomhene@gmail.com>
2022-06-20 17:08:19 +0200

    Deleted test in wrong class

--- src/test/java/de/htw/webtech/CardRestControllerTest.java
+++ src/test/java/de/htw/webtech/CardRestControllerTest.java
@@ -51,29 +51,5 @@ public class CardRestControllerTest {
 
     }
 
-    @Test
-    public void testGetAlbum() throws Exception {
-        // Test data and service moc
-        Card c1 = new Card("flasche", "bottle");
-        Card c2 = new Card("Dino", "Trex");
-        Album a1 = new Album("album");
-        a1.setId(2L);
-        c1.setId(24L);
-        c1.setAlbum(a1);
-        c2.setId(25L);
-        c2.setAlbum(a1);
-        when(albumService.get(a1.getAlbumId())).thenReturn(a1);
-
-        // Expected Result
-        String expected = "{\"id\":24,\"frontInformation\":\"flasche\",\"backInformation\":\"bottle\"}" +
-                "{\"id\":25,\"frontInformation\":\"Dino\",\"backInformation\":\"Trex\"}";
-
-        // Call and comparison
-        this.mockMvc.perform(get("/album/1"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().string(containsString(expected)));
-
-    }
 }
 
